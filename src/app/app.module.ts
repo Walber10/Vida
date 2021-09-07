afe658858dcb3568bb822ea1428f18c13f745a8e
WalberAmorim10 <walber@instarent.com.au>
2021-09-07 16:36:28 +1000

    vida-pilates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { BookingComponent } from './component/booking/booking.component';
+import { StudioComponent } from './component/studio/studio.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { LoginComponent } from './component/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { MoreComponent } from './more/more.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    BookingComponent,
+    StudioComponent,
+    ContactComponent,
+    LoginComponent,
+    MoreComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    BrowserAnimationsModule ,
+    NgImageSliderModule,
+    GoogleMapsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
+
   ],
+     
+
   providers: [],
   bootstrap: [AppComponent]
 })
