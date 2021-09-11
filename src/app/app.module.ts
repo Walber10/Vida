3f310cf54a10f5a652d97b94ce701ad6190ffad8
WalberAmorim10 <walberamorimsp@gmail.com>
2021-09-11 10:38:04 +1000

    teste4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { AboutV2Component } from './component/about-v2/about-v2.component';
+import { BookingComponent } from './component/booking/booking.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ContentComponent } from './component/content/content.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { StudioComponent } from './component/studio/studio.component';
+import { MoreComponent } from './component/more/more.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { NgImageSliderModule } from 'ng-image-slider';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    AboutV2Component,
+    BookingComponent,
+    ContactComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    StudioComponent,
+    MoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    FormsModule,
+    NgImageSliderModule
+  
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  responsive = ''
+  toggles = ''
+  togleNav: boolean = true
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addMobile() {
+    this.togleNav = !this.togleNav
+    
+    if(this.responsive == 'responsive') {
+      this.responsive = '' 
+      this.toggles = ''
+    } else {
+      this.responsive = 'responsive'
+      this.toggles = 'togleNav'
+    }
+
+    
+  }
+
+}
