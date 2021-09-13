15d9128543aa74e7dcfc4c37225884181cc608d8
WalberAmorim10 <walberamorimsp@gmail.com>
2021-09-13 11:43:13 +1000

    update navbar

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -15,6 +15,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.togleNav)
   }
 
   addMobile() {
@@ -27,7 +28,7 @@ export class HeaderComponent implements OnInit {
       this.responsive = 'responsive'
       this.toggles = 'togleNav'
     }
-
+console.log(this.togleNav)
     
   }
 
